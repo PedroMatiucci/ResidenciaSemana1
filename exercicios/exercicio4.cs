67a68038fbb552debd51d8cea6ac0bfab3fade52
Pedro Pereira <pmatiucci@gmail.com>
2022-11-17 14:42:46 -0300

    Exercicio 4

--- exercicios/exercicio4.cs
+++ exercicios/exercicio4.cs
@@ -1,13 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using exercicio1;
+using exercicio2;
 
 namespace exercicio4
 
 {
     internal class Poligono
     {
+        private List<Vertice> array_vertices = new List<Vertice>();
+
+        public List<Vertice> Array_vertices
+        {
+            get
+            {
+                return array_vertices;
+            }
+            set
+            {
+                if (array_vertices.Count - 1 < 3)
+                {
+                    throw new ImpossibleTriangleExeption("Este Triangulo e Impossivel");
+                }
+            }
+        }
+
+        public bool AddVertice(Vertice novo_vertice)
+        {
+            if (Array_vertices.Contains(novo_vertice))
+            {
+                return false;
+            }
+            else
+            {
+                Array_vertices.Add(novo_vertice);
+                return true;
+            }
+        }
+
+        public void RemovoeVertice(Vertice novo_vertice) {
+            if (Array_vertices.Contains(novo_vertice))
+            {
+                Array_vertices.Remove(novo_vertice);
+            }
+        }
     }
 }
